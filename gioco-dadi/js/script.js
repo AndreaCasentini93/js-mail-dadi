1f7194207153492e983c43fe92537b78a11b07c3
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-04-28 15:36:16 +0200

    GIOCO-DADI CSS Refactoring

--- gioco-dadi/js/script.js
+++ gioco-dadi/js/script.js
@@ -9,6 +9,34 @@ var computerNumber = Math.floor ((Math.random() * 6) + 1);
 console.log(computerNumber);
 
 // 3) Stabilire il vincitore, in base a chi fa il punteggio più alto
+if (userNumber == 1) {
+    document.getElementById("user").innerHTML = "<img src=\"img/dado-1.png\" alt=\"Image Dice\">"
+} else if (userNumber == 2) {
+    document.getElementById("user").innerHTML = "<img src=\"img/dado-2.png\" alt=\"Image Dice\">"
+} else if (userNumber == 3) {
+    document.getElementById("user").innerHTML = "<img src=\"img/dado-3.png\" alt=\"Image Dice\">"
+} else if (userNumber == 4) {
+    document.getElementById("user").innerHTML = "<img src=\"img/dado-4.png\" alt=\"Image Dice\">"
+} else if (userNumber == 5) {
+    document.getElementById("user").innerHTML = "<img src=\"img/dado-5.png\" alt=\"Image Dice\">"
+} else if (userNumber == 6) {
+    document.getElementById("user").innerHTML = "<img src=\"img/dado-6.png\" alt=\"Image Dice\">"
+}
+
+if (computerNumber == 1) {
+    document.getElementById("computer").innerHTML = "<img src=\"img/dado-1.png\" alt=\"Image Dice\">"
+} else if (computerNumber == 2) {
+    document.getElementById("computer").innerHTML = "<img src=\"img/dado-2.png\" alt=\"Image Dice\">"
+} else if (computerNumber == 3) {
+    document.getElementById("computer").innerHTML = "<img src=\"img/dado-3.png\" alt=\"Image Dice\">"
+} else if (computerNumber == 4) {
+    document.getElementById("computer").innerHTML = "<img src=\"img/dado-4.png\" alt=\"Image Dice\">"
+} else if (computerNumber == 5) {
+    document.getElementById("computer").innerHTML = "<img src=\"img/dado-5.png\" alt=\"Image Dice\">"
+} else if (computerNumber == 6) {
+    document.getElementById("computer").innerHTML = "<img src=\"img/dado-6.png\" alt=\"Image Dice\">"
+}
+
 if (userNumber > computerNumber) {
     document.getElementById("message").innerHTML = "Congratulations, you won!"
 } else if (userNumber < computerNumber) {
