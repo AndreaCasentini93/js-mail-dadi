e8c85fdb60a4772b38734f4224808c614c8c7668
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-04-28 13:53:03 +0200

    MAIL Refactoring

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,5 +1,5 @@
 // 1) Chiedi all'utente la sua e-mail
-var userMail = prompt("Write your e-mail, please.");
+var userMail = prompt("Insert your e-mail, please.");
 
 // 2) Controlla che sia nella lista di chi può partecipare
 var mailList = ["blue@gmail.com", "green@gmail.com", "yellow@gmail.com", "orange@gmail.com", "pink@gmail.com", "brown@gmail.com", "red@gmail.com", "black@gmail.com", "purple@gmail.com", "white@gmail.com"];
@@ -16,7 +16,7 @@ for (var i = 0; i < mailList.length; i++) {
 
 // 3) Stampa un messaggio appropriato sull'esito del controllo
 if (k > 0) {
-    document.getElementById("message").innerHTML = "Welcome! Your email is on the guest list."
+    document.getElementById("message").innerHTML = "Welcome! Your e-mail is on the guest list."
 } else if (k == 0) {
-    document.getElementById("message").innerHTML = "Sorry... Your email is not on the guest list."
+    document.getElementById("message").innerHTML = "Sorry... Your e-mail isn't on the guest list."
 }
